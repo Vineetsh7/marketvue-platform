8c4ddd0289115ffb532f251cd80c4eda75af934f
Vineetsh7 <xyz.meoutlook010@gmail.com>
2026-01-13 21:18:32 +0530

    implemented header navigation and userdownmenu with constant file

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = async () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Signalist logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto cursor-pointer"
+          />
+        </Link>
+        <nav className="hidden sm:block"></nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
