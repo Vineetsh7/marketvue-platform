9d4445ccf9f6ab40d83bf4e9bf3788a475afa36f
Vineetsh7 <xyz.meoutlook010@gmail.com>
2026-02-08 15:22:07 +0530

    Improves auth layout and sign-in page
    Refactors the authentication layout to redirect authenticated users, improving user experience.
    
    Updates sign-in page to correctly implement email sign-in functionality.
    
    Corrects a typo in the testimonial section and updates the author's name.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
+import {auth} from "@/lib/better-auth/auth";
 import {headers} from "next/headers";
 import {redirect} from "next/navigation";
 
 const Layout = async ({ children }: { children : React.ReactNode }) => {
+    const session = await auth.api.getSession({ headers: await headers() })
 
+    if(session?.user) redirect('/')
 
     return (
         <main className="auth-layout">
             <section className="auth-left-section scrollbar-hide-default">
                 <Link href="/" className="auth-logo">
-                    <Image src="/assets/icons/logo.svg" alt="marketvue logo" width={140} height={32} className='h-8 w-auto' />
+                    <Image src="/assets/icons/logo.svg" alt="MarketVue logo" width={140} height={32} className='h-8 w-auto' />
                 </Link>
 
                 <div className="pb-6 lg:pb-8 flex-1">{children}</div>
@@ -19,11 +22,11 @@ const Layout = async ({ children }: { children : React.ReactNode }) => {
             <section className="auth-right-section">
                 <div className="z-10 relative lg:mt-4 lg:mb-16">
                     <blockquote className="auth-blockquote">
-                       MarketVue has transformed my investment strategy by turning my watchlist into a series of actionable opportunities. The precision of their alerts has significantly increased my market confidence and streamlined my decision-making process.market
+                        MarkeetVue turned my watchlist into a winning list. The alerts are spot-on, and I feel more confident making moves in the market
                     </blockquote>
                     <div className="flex items-center justify-between">
                         <div>
-                            <cite className="auth-testimonial-author">- Eren R.</cite>
+                            <cite className="auth-testimonial-author">- Ethan R.</cite>
                             <p className="max-md:text-xs text-gray-500">Retail Investor</p>
                         </div>
                         <div className="flex items-center gap-0.5">
